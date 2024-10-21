f91245836c9a178f3f08bba7919f067e2c70bcc9
IroshanRathnayake <meet.iroshan@gmail.com>
2024-10-21 11:23:22 +0530

    chore[structure]: initialize the base project structure of prescription-mgmt

---
+++ src/app/prescription-management/component/prescription-data-entry/prescription-data-entry.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prescription-data-entry',
+  standalone: true,
+  imports: [],
+  templateUrl: './prescription-data-entry.component.html',
+  styleUrl: './prescription-data-entry.component.css'
+})
+export class PrescriptionDataEntryComponent {
+
+}
