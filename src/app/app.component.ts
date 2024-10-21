0d46f0bde846e30e4aec47b5246593d605ffaf31
kavindujayakody <kavindusankalpa791@gmail.com>
2024-10-21 17:24:20 +0530

    dashbord desing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MedicalRecordsHandlingComponent } from './medical-records-handling/medical-records-handling.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MedicalRecordsHandlingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/medical-records-handling/medical-records-handling.component.ts
+++ src/app/medical-records-handling/medical-records-handling.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DashbordComponent } from './dashbord/dashbord.component';
 
 @Component({
   selector: 'app-medical-records-handling',
   standalone: true,
-  imports: [],
+  imports: [DashbordComponent],
   templateUrl: './medical-records-handling.component.html',
   styleUrl: './medical-records-handling.component.css'
 })
