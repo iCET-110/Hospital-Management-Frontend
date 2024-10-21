cdb28fb60e98da79894c621e083bd82ae7287a00
IroshanRathnayake <meet.iroshan@gmail.com>
2024-10-21 11:29:28 +0530

    chore[structure]: update structure

---
+++ src/app/prescription-management/component/prescription-data-entry/confirmation-popup/confirmation-popup.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation-popup',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirmation-popup.component.html',
+  styleUrl: './confirmation-popup.component.css'
+})
+export class ConfirmationPopupComponent {
+
+}
