19802371a968836f4993a47db360753bbf0f4b12
nihindug2004 <nihindug2004@gmail.com>
2024-10-21 17:04:06 +0530

    Initial Stage - Patient Search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './patient-management/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/patient-management/search/search.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Patient {
+  name: string;
+  nic: string;
+  dob: string;
+  gender: string;
+  contact: string;
+  residentAddress: string;
+  guardian: string;
+  guardianContact: string;
+  currentlyAdmitted: boolean;
+  medicalInfo: {
+    bloodType: string;
+    allergies: string[];
+    remarks: string;
+  }
+}
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+  searchTerm: string = '';
+  selectedPatient: Patient = {
+    name: 'Shane Ranasinghe',
+    nic: '200322450351',
+    dob: '2003/10/20',
+    gender: 'male',
+    contact: '078 817 9855',
+    residentAddress: '21/A, Cinnamon watta Road, Colombo 07',
+    guardian: 'Mr. Ranasinghe',
+    guardianContact: '071 172 9692',
+    currentlyAdmitted: true,
+    medicalInfo: {
+      bloodType: 'O+',
+      allergies: ['pineapple', 'pretty girls'],
+      remarks: 'left wrist fracture, undergone leg surgery in 2020 & a broken heart.'
+    }
+  };
+
+  onSearch(): void {
+    // Implement search logic here
+    console.log('Searching for:', this.searchTerm);
+  }
+}
