e42f861f0407aaeb7c54708a1c9ee448e4b01b00
DulangaMendis <dulangamendis82@gmail.com>
2024-10-21 16:55:29 +0530

    develop view records form's body

--- src/app/medical-records-handling/view-records-reports/view-records-reports.component.ts
+++ src/app/medical-records-handling/view-records-reports/view-records-reports.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DulangaComponent } from './dulanga/dulanga.component';
 
 @Component({
   selector: 'app-view-records-reports',
   standalone: true,
-  imports: [],
+  imports: [DulangaComponent],
   templateUrl: './view-records-reports.component.html',
   styleUrl: './view-records-reports.component.css'
 })
