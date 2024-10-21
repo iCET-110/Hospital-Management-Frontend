33176c42fc3bf7c92a703f34d6d48e304590bd68
devinda9936 <devindanishshanka2003@gmail.com>
2024-10-21 14:51:32 +0100

    bootstrap install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ViewComponent } from './medical-records-handling/view/view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
