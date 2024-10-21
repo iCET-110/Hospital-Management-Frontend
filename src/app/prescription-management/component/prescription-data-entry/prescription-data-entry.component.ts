f1ab404083248f85649baed9024684646b175060
mithu0621 <mithunisk03@gmail.com>
2024-10-21 16:49:40 +0530

    confirmation popup

--- src/app/prescription-management/component/prescription-data-entry/prescription-data-entry.component.ts
+++ src/app/prescription-management/component/prescription-data-entry/prescription-data-entry.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ConfirmationPopupComponent } from './confirmation-popup/confirmation-popup.component';
 
 @Component({
   selector: 'app-prescription-data-entry',
   standalone: true,
-  imports: [],
+  imports: [ConfirmationPopupComponent],
   templateUrl: './prescription-data-entry.component.html',
   styleUrl: './prescription-data-entry.component.css'
 })
