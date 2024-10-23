9f33a2c0781ab0585d9a15f166ba9506ade6e58a
AnudaIsuranga <isuranuda555@gmail.com>
2024-10-23 11:29:08 +0530

    feat[Prescription Data Entry UI]: implement Presciption Data Entry UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { PrescriptionDataEntryComponent } from './prescription-management/component/prescription-data-entry/prescription-data-entry.component';
+import { Prescription } from './prescription-management/model/prescription';
+import { PrescriptionManagementComponent } from './prescription-management/prescription-management.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PrescriptionManagementComponent 
+  },
+ 
+];
