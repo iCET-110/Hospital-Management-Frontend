bc32da3f3085e4bb64f44ef1d350209bb22e2293
Anushka Eshan <eanushka91@gmail.com>
2024-10-23 16:42:50 +0530

    add category implimentation done

--- src/app/appointment-handling/add-category/add-category.component.ts
+++ src/app/appointment-handling/add-category/add-category.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-category',
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
   templateUrl: './add-category.component.html',
   styleUrl: './add-category.component.css'
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnInit {
+  currentDate: string;
+  adminName: string;
+  adminRole: string;
+  categoryId: string;
+  categoryDescription: string;
 
+  constructor() {
+    this.currentDate = '10/18/2024'; 
+    this.adminName = 'Nisila Perera';
+    this.adminRole = 'Admin';
+    this.categoryId = 'CA001';
+    this.categoryDescription = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you';
+  }
+
+  ngOnInit(): void {
+    document.body.style.backgroundColor = '#E6FFE8';
+    
+  }
+
+  addCategory(): void {
+    console.log('Adding category:', this.categoryId, this.categoryDescription);
+  }
 }
