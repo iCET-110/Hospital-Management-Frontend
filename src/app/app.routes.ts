26518637f9bf3677bae561de34f4464dd7772fd2
icet-sharadha <icet.sharadha@gmail.com>
2024-10-23 11:27:11 +0530

    chane nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,5 @@ import { Routes } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:NavBarComponent
-    },
+
 ];
