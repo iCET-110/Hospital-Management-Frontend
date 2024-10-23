227e196fa76eb377d77aa0f593b042256288510b
DHJ99 <dananjayamadushanka825@gmail.com>
2024-10-23 13:42:14 +0530

    Background color

--- src/app/patient-management/view-patient-list/view-patient-list.component.spec.ts
+++ src/app/patient-management/view-patient-list/view-patient-list.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ViewPatientListComponent } from './view-patient-list.component';
 
 describe('ViewPatientListComponent', () => {
--- src/app/patient-management/view-patient-list/view-patient-list.component.ts
+++ src/app/patient-management/view-patient-list/view-patient-list.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from '../../common/nav-bar/nav-bar.component'
+
+interface Patient {
+  name: string;
+  nic: string;
+  dob: string;
+  gender: string;
+  contact: string;
+  address: string;
+  guardian: string;
+  guardianContact: string;
+}
 
 @Component({
   selector: 'app-view-patient-list',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule , NavBarComponent], 
   templateUrl: './view-patient-list.component.html',
-  styleUrl: './view-patient-list.component.css'
+  styleUrls: ['./view-patient-list.component.css']
 })
 export class ViewPatientListComponent {
-
+  patients: Patient[] = [
+    {
+      name: "John Doe",
+      nic: "123456789V",
+      dob: "1990-05-15",
+      gender: "Male",
+      contact: "+94 771234567",
+      address: "123 Main St, Colombo",
+      guardian: "Jane Doe",
+      guardianContact: "+94 777654321"
+    }
+    // Other patient records...
+  ];
 }
