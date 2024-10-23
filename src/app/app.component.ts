5cde5610290e3670f0b4ab1ea90139c920865240
yysamarawickrama <yysamarawickrama@gmail.com>
2024-10-23 11:20:22 +0530

    add records header component done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AppointmentHandlingComponent } from "./appointment-handling/appointment-handling.component";
-import { MedicalRecordsHandlingComponent } from "./medical-records-handling/medical-records-handling.component";
-import { PatientManagementComponent } from "./patient-management/patient-management.component";
-import { PrescriptionManagementComponent } from "./prescription-management/prescription-management.component";
+import { YalinaComponent } from './medical-records-handling/add-records/yalina/yalina.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent],
+  imports: [RouterOutlet,YalinaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/medical-records-handling/add-records/add-records.component.ts
+++ src/app/medical-records-handling/add-records/add-records.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { YalinaComponent } from "./yalina/yalina.component";
 
 @Component({
   selector: 'app-add-records',
   standalone: true,
-  imports: [],
+  imports: [YalinaComponent],
   templateUrl: './add-records.component.html',
   styleUrl: './add-records.component.css'
 })
