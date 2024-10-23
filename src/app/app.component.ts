cc6460a00b09d2418db1d16aa59f8cb45b2e5cc4
IsurikaDaniel <d.nethmiisurika@gmail.com>
2024-10-23 09:44:28 +0530

    view appoinment implimentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppointmentViewComponent } from './appointment-view/appointment-view.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AppointmentViewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/appointment-view/appointment-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './appointment-view.component.html',
+  styleUrl: './appointment-view.component.css'
+})
+export class AppointmentViewComponent {
+
+}
