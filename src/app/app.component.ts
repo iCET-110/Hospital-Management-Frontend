96007e31fd83d41311f222f1e80caaac8009741a
kavindujayakody <kavindusankalpa791@gmail.com>
2024-10-23 11:46:23 +0530

    dashboard-html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { AppointmentHandlingComponent } from "./appointment-handling/appointment
 import { MedicalRecordsHandlingComponent } from "./medical-records-handling/medical-records-handling.component";
 import { PatientManagementComponent } from "./patient-management/patient-management.component";
 import { PrescriptionManagementComponent } from "./prescription-management/prescription-management.component";
+import { MainDashboardComponent } from './medical-records-handling/main-dashboard/main-dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent],
+  imports: [RouterOutlet,MainDashboardComponent, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/medical-records-handling/medical-records-handling.component.ts
+++ src/app/medical-records-handling/medical-records-handling.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { DashbordComponent } from './main-dashboard/dashbord/dashbord.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 
 @Component({
   selector: 'app-medical-records-handling',
   standalone: true,
-  imports: [DashbordComponent],
+  imports: [],
   templateUrl: './medical-records-handling.component.html',
   styleUrl: './medical-records-handling.component.css'
 })
