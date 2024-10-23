cc48b9db808f952e66de4d316ffc57129955161a
DulangaMendis <dulangamendis82@gmail.com>
2024-10-23 17:03:03 +0530

    complete design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { MedicalRecordsHandlingComponent } from "./medical-records-handling/medi
 import { PatientManagementComponent } from "./patient-management/patient-management.component";
 import { PrescriptionManagementComponent } from "./prescription-management/prescription-management.component";
 import { ViewRecordsReportsComponent } from "./medical-records-handling/view-records-reports/view-records-reports.component";
+import { DulangaComponent } from "./medical-records-handling/view-records-reports/dulanga/dulanga.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent, ViewRecordsReportsComponent],
+  imports: [RouterOutlet, NavBarComponent, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent, ViewRecordsReportsComponent, DulangaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
