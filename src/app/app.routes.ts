8abc0b754cfe1be927539d1eac52bea62cf72598
anushkaindunil1 <anushkaindunil157@gmail.com>
2024-10-23 16:57:42 +0200

    add records header and Patient Report Details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MedicalRecordsHandlingComponent } from './medical-records-handling/medical-records-handling.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MedicalRecordsHandlingComponent }
+];
