b550f8ff485a8465a0a71d8f53bbc98e87870e3d
kavindujayakody <kavindusankalpa791@gmail.com>
2024-10-23 16:56:40 +0530

    main-dahboard-html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,11 @@ import { AppointmentHandlingComponent } from "./appointment-handling/appointment
 import { MedicalRecordsHandlingComponent } from "./medical-records-handling/medical-records-handling.component";
 import { PatientManagementComponent } from "./patient-management/patient-management.component";
 import { PrescriptionManagementComponent } from "./prescription-management/prescription-management.component";
-import { MainDashboardComponent } from './medical-records-handling/main-dashboard/main-dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent,MainDashboardComponent],
+  imports: [RouterOutlet,NavBarComponent, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
