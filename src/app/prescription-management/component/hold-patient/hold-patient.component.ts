cb42d01a016763eff832d564d791d0c818272bab
deshanii <deshanithilakshana2004@gmail.com>
2024-10-23 15:09:19 +0530

    added object array

--- src/app/prescription-management/component/hold-patient/hold-patient.component.ts
+++ src/app/prescription-management/component/hold-patient/hold-patient.component.ts
@@ -1,12 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hold-patient',
   standalone: true,
-  imports: [],
+  imports: [FormsModule , CommonModule],
   templateUrl: './hold-patient.component.html',
   styleUrl: './hold-patient.component.css'
 })
 export class HoldPatientComponent {
-  deleteIcon:string = "https://img.icons8.com/ios-glyphs/24/FFFFFF/filled-trash.png";
+
+   public HoldPatient =[
+  
+    { 
+      "patientID":"P001" , 
+      "patientName": "Kamal", 
+      "appoinmentID":"A001" 
+    },
+    { 
+      "patientID":"P002" , 
+      "patientName": "Ann", 
+      "appoinmentID":"A002" 
+    },
+    { 
+      "patientID":"P003" , 
+      "patientName": "Nimali", 
+      "appoinmentID":"A003" 
+    },
+    { 
+      "patientID":"P004" , 
+      "patientName": "Saman", 
+      "appoinmentID":"A004" 
+    },
+    { 
+      "patientID":"P005" , 
+      "patientName": "Andrew", 
+      "appoinmentID":"A005" 
+    },
+    { 
+      "patientID":"P006" , 
+      "patientName": "vimal", 
+      "appoinmentID":"A006" 
+    },
+    { 
+      "patientID":"P007" , 
+      "patientName": "John", 
+      "appoinmentID":"A007" 
+    }
+];
+
+
+
+
 }
