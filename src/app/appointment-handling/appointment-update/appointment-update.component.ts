3739fc17dc1775319a1596a0e4d80a57c65dc4e6
Binura Namulitha <binura.bin02@gmail.com>
2024-10-23 11:38:00 +0530

    test commit

--- src/app/appointment-handling/appointment-update/appointment-update.component.ts
+++ src/app/appointment-handling/appointment-update/appointment-update.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "../../common/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-appointment-update',
   standalone: true,
-  imports: [],
+  imports: [NavBarComponent],
   templateUrl: './appointment-update.component.html',
   styleUrl: './appointment-update.component.css'
 })
