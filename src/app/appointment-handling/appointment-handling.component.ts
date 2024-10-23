d9812b4a1f4b8b3e17ff7212de9a760541cf47a4
Anushka Eshan <eanushka91@gmail.com>
2024-10-23 14:43:06 +0530

    add-category test commit

--- src/app/appointment-handling/appointment-handling.component.ts
+++ src/app/appointment-handling/appointment-handling.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AppointmentSearchComponent } from "./appointment-search/appointment-search.component";
+import { AddCategoryComponent } from './add-category/add-category.component';
 
 @Component({
   selector: 'app-appointment-handling',
   standalone: true,
-  imports: [AppointmentSearchComponent],
+  imports: [AppointmentSearchComponent,AddCategoryComponent],
   templateUrl: './appointment-handling.component.html',
   styleUrl: './appointment-handling.component.css'
 })
