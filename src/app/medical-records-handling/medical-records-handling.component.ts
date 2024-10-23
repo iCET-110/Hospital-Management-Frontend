450268c0f9479aac84a24e4ac167a07f79c352ea
yysamarawickrama <yysamarawickrama@gmail.com>
2024-10-23 16:19:42 +0530

    update 2

--- src/app/medical-records-handling/medical-records-handling.component.ts
+++ src/app/medical-records-handling/medical-records-handling.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import{ AddRecordsComponent } from './add-records/add-records.component';
 
 @Component({
   selector: 'app-medical-records-handling',
   standalone: true,
-  imports: [AddRecordsComponent],
+  imports: [],
   templateUrl: './medical-records-handling.component.html',
   styleUrl: './medical-records-handling.component.css'
 })
