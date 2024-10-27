cfc8591854192d736158335fc76cb47170a0ad36
SadeepaNHerath <sadeepahearth@gmail.com>
2024-10-27 11:05:18 +0530

    routing added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
-import { AppointmentHandlingComponent } from "./appointment-handling/appointment-handling.component";
-import { MedicalRecordsHandlingComponent } from "./medical-records-handling/medical-records-handling.component";
-import { PatientManagementComponent } from "./patient-management/patient-management.component";
-import { PrescriptionManagementComponent } from "./prescription-management/prescription-management.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent],
+  imports: [RouterOutlet,NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
