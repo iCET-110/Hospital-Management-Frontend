d41baf281f718d58256386a73b04e7455ea20bb0
dilshankavishka <dilshankavishka888@gmail.com>
2024-10-31 15:25:23 +0530

    feat[Resume-Button-Functionality]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { HoldPatientComponent } from './prescription-management/component/hold-patient/hold-patient.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HoldPatientComponent
+    }  
+
+];
--- src/app/prescription-management/component/hold-patient/hold-patient.component.ts
+++ src/app/prescription-management/component/hold-patient/hold-patient.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-hold-patient',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports: [HoldPatientComponent],
   templateUrl: './hold-patient.component.html',
-  styleUrl: './hold-patient.component.css'
+  styleUrls: ['./hold-patient.component.css']
+  
 })
 export class HoldPatientComponent {
+
+  constructor(private http: HttpClient) {}
+
+  private apiUrl = '/api/v1/prescription';
+
+  // Function to handle the "Resume" button click
+  async resumePrescription(patientId: number): Promise<void> {
+    try {
+      // Fetch patient data from API
+      const patientData = await firstValueFrom(this.http.get(`${this.apiUrl}/${patientId}`));
+      this.displayPatientData(patientData);
+
+      // Delete hold prescription when resume button clicked
+      await firstValueFrom(this.http.delete(`${this.apiUrl}/hold-prescription?id=${patientId}`));
+       console.log('Patient resumed successfully');
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+  displayPatientData(data: any) {
+    // Logic to update patient data on the page
+    //
+    //
+    //
+    //insert code to get ID's of text input fields from the html and set values
+    //
+    //
+    console.log('Patient Data:', data);
+  }
 
 }
