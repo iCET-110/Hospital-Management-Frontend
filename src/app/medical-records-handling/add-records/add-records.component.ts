07f08d9e31e66599dbfe74c7074809c7bccd3733
SadeepaNHerath <sadeepahearth@gmail.com>
2024-10-31 20:07:27 +0530

    Update add-records.component.ts

--- src/app/medical-records-handling/add-records/add-records.component.ts
+++ src/app/medical-records-handling/add-records/add-records.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { DilshaniComponent } from './dilshani/dilshani.component';
 import { YalinaComponent } from "./yalina/yalina.component";
-import { SaradaComponent } from "../view-records-reports/sarada/sarada.component";
 
 @Component({
   selector: 'app-add-records',
   standalone: true,
-  imports: [DilshaniComponent, YalinaComponent, SaradaComponent],
+  imports: [DilshaniComponent, YalinaComponent],
   templateUrl: './add-records.component.html',
   styleUrl: './add-records.component.css'
 })
