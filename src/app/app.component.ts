560eeb9ebb8c286b5d0a18f08f06aaeab6667117
SaradaThisun <saradathisun06@gmail.com>
2024-10-31 16:53:05 +0530

    Records TS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { ViewRecordsReportsComponent } from './medical-records-handling/view-records-reports/view-records-reports.component';
+import { SaradaComponent } from "./medical-records-handling/view-records-reports/sarada/sarada.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, ViewRecordsReportsComponent, SaradaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/medical-records-handling/view-records-reports/sarada/sarada.component.ts
+++ src/app/medical-records-handling/view-records-reports/sarada/sarada.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './sarada.component.css'
 })
 export class SaradaComponent {
+  recordId: string | null = null;
+  date: string | null = null;
 
+   applyForm() {
+  
+   }
+
+  clearForm() {
+
+    this.recordId = null;
+    this.date = null;
+
+    (document.getElementById('record-id') as HTMLInputElement).value = '';
+    (document.getElementById('date') as HTMLInputElement).value = '';
+    
+  }
 }
+
+
+
+
