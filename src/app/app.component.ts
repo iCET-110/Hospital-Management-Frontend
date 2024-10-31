f7a6c324ced2bafa028468fc3622c109c40d4ae2
devinda9936 <devindanishshanka2003@gmail.com>
2024-10-31 16:24:27 +0100

    VIEW TABLE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { DevindaComponent } from "./medical-records-handling/view-reports/devinda/devinda.component";
+import { ViewReportsComponent } from "./medical-records-handling/view-reports/view-reports.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, DevindaComponent, ViewReportsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
