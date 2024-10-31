f721025c58e63cbf0f805e776402c5a78287d7ea
kavindujayakody <kavindusankalpa791@gmail.com>
2024-10-31 16:31:40 +0530

    records-main-dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
-import { AppointmentHandlingComponent } from "./appointment-handling/appointment-handling.component";
-import { MedicalRecordsHandlingComponent } from "./medical-records-handling/medical-records-handling.component";
-import { PatientManagementComponent } from "./patient-management/patient-management.component";
-import { PrescriptionManagementComponent } from "./prescription-management/prescription-management.component";
-
+import { MainDashboardComponent } from "./medical-records-handling/main-dashboard/main-dashboard.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent],
+  imports: [RouterOutlet, NavBarComponent, MainDashboardComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
