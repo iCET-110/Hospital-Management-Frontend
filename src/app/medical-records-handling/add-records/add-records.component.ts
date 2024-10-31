971307f14c3dd806b8a7d2de35da24539be8324f
SaradaThisun <saradathisun06@gmail.com>
2024-10-31 16:49:59 +0530

    Records TS

--- src/app/medical-records-handling/add-records/add-records.component.ts
+++ src/app/medical-records-handling/add-records/add-records.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DilshaniComponent } from './dilshani/dilshani.component';
 import { YalinaComponent } from "./yalina/yalina.component";
+import { SaradaComponent } from "../view-records-reports/sarada/sarada.component";
 
 @Component({
   selector: 'app-add-records',
   standalone: true,
-  imports: [DilshaniComponent, YalinaComponent],
+  imports: [DilshaniComponent, YalinaComponent, SaradaComponent],
   templateUrl: './add-records.component.html',
   styleUrl: './add-records.component.css'
 })
