dee28a31f952c0204532a5f302affb04afccb918
Binura Namulitha <binura.bin02@gmail.com>
2024-10-31 14:24:53 +0530

    test cases done for update appointment

--- src/app/appointment-handling/appointment-update/appointment-update.component.ts
+++ src/app/appointment-handling/appointment-update/appointment-update.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { NavBarComponent } from "../../common/nav-bar/nav-bar.component";
+import { FormsModule, NgModel} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { clear } from 'console';
 
 @Component({
   selector: 'app-appointment-update',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './appointment-update.component.html',
   styleUrl: './appointment-update.component.css'
 })
 export class AppointmentUpdateComponent implements OnInit {
+
+  public searchText:any=""; 
+
   ngOnInit(): void {
     document.body.style.backgroundColor = '#E6FFE8';
   }
 
   public testAppointments = [
     {
-      appintmentId: '123',
+      appointmentId: '123',
       name: 'John Doe',
       nic: '123456789V',
       mobile: '0712345678',
@@ -39,4 +45,82 @@ export class AppointmentUpdateComponent implements OnInit {
       category: 'Dental'
     }
   ];
+
+  searchPatient(){
+    let apponitmentId = document.querySelector(".apponitmentId") as HTMLInputElement;
+    let txtName = document.querySelector(".txtName") as HTMLInputElement;
+    let txtNic = document.querySelector(".txtNic") as HTMLInputElement;
+    let txtMobile = document.querySelector(".txtMobile") as HTMLInputElement;
+    let txtEmail = document.querySelector(".txtEmail") as HTMLInputElement;
+    let aptDate = document.querySelector(".aptDate") as HTMLInputElement;
+    let aptTime = document.querySelector(".aptTime") as HTMLInputElement;
+    let txtCategory = document.querySelector(".txtCategory") as HTMLInputElement;
+    let txtDesc = document.querySelector(".txtDesc") as HTMLInputElement;
+    let genderChckBox = document.querySelector(".genderChckBox") as HTMLInputElement;
+
+    const foundAppointment = this.testAppointments.find(appointment => 
+      appointment.appointmentId === this.searchText || appointment.name === this.searchText
+    );
+    if (foundAppointment) {
+      apponitmentId.value = foundAppointment.appointmentId;
+      txtName.value = foundAppointment.name;
+      txtNic.value = foundAppointment.nic;
+      txtMobile.value = foundAppointment.mobile;
+      txtEmail.value = foundAppointment.email;
+      aptDate.value = foundAppointment.date;
+      aptTime.value = foundAppointment.time;
+      txtCategory.value = foundAppointment.category;
+      txtDesc.value = foundAppointment.description;
+      genderChckBox.checked = foundAppointment.gender;
+      this.searchText = "";
+    } else {
+      console.log("Appointment not found");
+      this.searchText = "";
+    }
+  }
+
+  update(){
+    let apponitmentId = document.querySelector(".apponitmentId") as HTMLInputElement;
+    let txtName = document.querySelector(".txtName") as HTMLInputElement;
+    let txtNic = document.querySelector(".txtNic") as HTMLInputElement;
+    let txtMobile = document.querySelector(".txtMobile") as HTMLInputElement;
+    let txtEmail = document.querySelector(".txtEmail") as HTMLInputElement;
+    let aptDate = document.querySelector(".aptDate") as HTMLInputElement;
+    let aptTime = document.querySelector(".aptTime") as HTMLInputElement;
+    let txtCategory = document.querySelector(".txtCategory") as HTMLInputElement;
+    let txtDesc = document.querySelector(".txtDesc") as HTMLInputElement;
+    let genderChckBox = document.querySelector(".genderChckBox") as HTMLInputElement;
+
+    const foundAppointmentIndex = this.testAppointments.findIndex(appointment => 
+      appointment.appointmentId === apponitmentId.value
+    );
+    if (foundAppointmentIndex !== -1) {
+      this.testAppointments[foundAppointmentIndex] = {
+        appointmentId: apponitmentId.value,
+        name: txtName.value,
+        nic: txtNic.value,
+        mobile: txtMobile.value,
+        email: txtEmail.value,
+        date: aptDate.value,
+        time: aptTime.value,
+        category: txtCategory.value,
+        description: txtDesc.value,
+        gender: genderChckBox.checked,
+      };
+      apponitmentId.value = "";
+      txtName.value = "";
+      txtNic.value = "";
+      txtMobile.value = "";
+      txtEmail.value = "";
+      aptDate.value = "";
+      aptTime.value = "";
+      txtCategory.value = "";
+      txtDesc.value = "";
+      genderChckBox.checked = false;
+      this.searchText = "";
+    } else {
+      console.log("Appointment not found");
+      this.searchText = "";
+    }
+  }
 }
