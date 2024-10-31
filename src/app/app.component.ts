59df0e19a74f61a96efd5e488caf1acd65597079
DulangaMendis <dulangamendis82@gmail.com>
2024-10-31 11:41:09 +0530

    pull origin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { DulangaComponent } from "./medical-records-handling/view-records-reports/dulanga/dulanga.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, DulangaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
