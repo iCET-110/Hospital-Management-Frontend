42e024862a82891402b846be019c4c62b8393e56
pawaninanayakkara <nanayakkara2004@gmail.com>
2024-10-31 16:17:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { AppointmentHandlingComponent } from "./appointment-handling/appointment
 import { MedicalRecordsHandlingComponent } from "./medical-records-handling/medical-records-handling.component";
 import { PatientManagementComponent } from "./patient-management/patient-management.component";
 import { PrescriptionManagementComponent } from "./prescription-management/prescription-management.component";
+import { PatientInfoComponent } from "./prescription-management/component/patient-info/patient-info.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent],
+  imports: [RouterOutlet, AppointmentHandlingComponent, MedicalRecordsHandlingComponent, PatientManagementComponent, PrescriptionManagementComponent, PatientInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
