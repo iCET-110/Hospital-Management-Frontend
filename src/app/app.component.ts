880d6c729fd5bc064db5b80495143ba900dae9c3
SachinthaTharukaLakshan <sachinthat03@gamil.com>
2024-10-31 16:44:04 +0530

    new css edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { MainDashboardComponent } from "./medical-records-handling/main-dashboard/main-dashboard.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, MainDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
