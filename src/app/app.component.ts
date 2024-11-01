84ee0e2d2528255296b45b0eb406699f30bff819
SACHISUCHI <sachiniabhimanisamarakoon@gmail.com>
2024-11-01 13:30:15 +0530

    New Update - Responsived the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
+import { SachiniComponent } from "./medical-records-handling/view-records-reports/sachini/sachini.component";
+import { DulangaComponent } from "./medical-records-handling/view-records-reports/dulanga/dulanga.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, SachiniComponent, DulangaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/medical-records-handling/view-records-reports/sachini/sachini.component.ts
+++ src/app/medical-records-handling/view-records-reports/sachini/sachini.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-sachini',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sachini.component.html',
   styleUrl: './sachini.component.css'
 })
 export class SachiniComponent {
-
-}
+  isModalOpen = false;
+  selectedRecord: { id: string; date: string; time: string } | null = null;
+} 
