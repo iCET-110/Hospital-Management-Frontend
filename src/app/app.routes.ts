f1b3cefebecc269733dabe44a5af893235b264fd
icet-sharadha <icet.sharadha@gmail.com>
2024-11-11 16:12:44 +0530

    routing appointment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 
-
-export const routes: Routes = [
 import { UpdateComponent } from './patient-management/update/update.component';
 import { NavBarComponent } from './common/nav-bar/nav-bar.component';
 import { DashBordPageComponent } from './dash-bord-page/dash-bord-page.component';
@@ -9,6 +7,10 @@ import { ViewPatientListComponent } from './patient-management/view-patient-list
 import { PatientManagementComponent } from './patient-management/patient-management.component';
 import { SearchComponent } from './patient-management/search/search.component';
 import { AddComponent } from './patient-management/add/add.component';
+import { AddAppointmentComponent } from './appointment-handling/add-appointment/add-appointment.component';
+import { DeleteAppointmentComponent } from './appointment-handling/delete-appointment/delete-appointment.component';
+import { AppointmentViewComponent } from './appointment-handling/appointment-view/appointment-view.component';
+import { AppoimentSearchComponent } from './appointment-handling/appoiment-search/appoiment-search.component';
 
 export const routes: Routes = [
     {
@@ -30,6 +32,22 @@ export const routes: Routes = [
             {
                 path:"update-patient",
                 component:UpdateComponent
+            },
+            {
+                path:"add-appointment",
+                component:AddAppointmentComponent
+            },
+            {
+                path:"delete-appointment",
+                component:DeleteAppointmentComponent
+            },
+            {
+                path:"view-appointments",
+                component:AppointmentViewComponent
+            },
+            {
+                path:"search-appointment",
+                component:AppoimentSearchComponent
             }
         ]
     },
